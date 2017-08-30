f724d80585f0da0e13b012a0d1c7b6b87a3809ec
shomiyata <eighteighttwenty@gmail.com>
2017-08-30 17:20:40 -0400

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -10,9 +10,22 @@ class TweetWall extends React.Component {
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  componentWillMount = () => {
+    this.setState({
+      tweets: this.props.newTweets
+    })
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    this.setState({
+      tweets: nextProps.newTweets.concat(this.state.tweets)
+
+    })
+  }
+  
+  shouldComponentUpdate = (nextProps) => {
+    return (nextProps.tweets)
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
